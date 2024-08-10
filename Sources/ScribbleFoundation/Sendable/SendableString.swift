98fa3d0474e3a6756753f42b6ab54cebb4fd063a
N3v1 <n3v1010@gmail.com>
2024-08-10 15:10:09 +0200

    (chore): fix unused bool warning in SendableString

--- Sources/ScribbleFoundation/Sendable/SendableString.swift
+++ Sources/ScribbleFoundation/Sendable/SendableString.swift
@@ -1,9 +1,33 @@
 //
-//  File.swift
+//  SendableString.swift
 //  ScribbleFoundation
 //
-//  Created by Nevio Hirani on 10.08.24.
+//  Copyright (c) 2024 ScribbleLabApp LLC. All rights reserved
 //
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are met:
+//
+//  1. Redistributions of source code must retain the above copyright notice, this
+//     list of conditions and the following disclaimer.
+//
+//  2. Redistributions in binary form must reproduce the above copyright notice,
+//     this list of conditions and the following disclaimer in the documentation
+//     and/or other materials provided with the distribution.
+//
+//  3. Neither the name of the copyright holder nor the names of its
+//     contributors may be used to endorse or promote products derived from
+//     this software without specific prior written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+//  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+//  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+//  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+//  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+//  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+//  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+//  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+//  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+//  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import Atomics
 import Foundation
@@ -34,13 +58,15 @@ public final class SendableString: Sendable {
                     let currentValue = self.queue.sync { self._value }
                     let newValue = mutate(currentValue)
                     
-                    if self.queue.sync(execute: {
-                        let result = self._value == currentValue
-                        if result {
+                    let updated = self.queue.sync {
+                        if self._value == currentValue {
                             self._value = newValue
+                            return true
                         }
-                        result
-                    }) {
+                        return false
+                    }
+                    
+                    if updated {
                         continuation.resume()
                         break
                     }
